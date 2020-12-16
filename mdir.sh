e936535f8ae14bd60e61e6cb9c276ae34835be5e
Ann <75046143+annazc-ann@users.noreply.github.com>
2020-12-16 11:37:51 +0700

    Add files via upload

---
+++ mdir.sh
@@ -0,0 +1,11 @@
+mkdir /sdcard/Download
+mkdir /sdcard/Download/Video
+mkdir /sdcard/Download/Music
+mkdir /sdcard/Download/Documents
+mkdir /sdcard/Download/Apps
+mkdir /sdcard/Download/Icons
+mkdir /sdcard/Download/Compressed
+mkdir /sdcard/Download/Others
+echo
+echo OK SIP!
+echo
