36437564da569269413b834a22d5bf41bad88ec3
Ann <75046143+annazc-ann@users.noreply.github.com>
2020-12-16 11:55:43 +0700

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -16,34 +16,31 @@ comps = ["zip","rar","tar","gz","iso","7z","bz2","jar","lzma"]
 # Change the current working directory(cwd) to the Download path
 os.chdir(dl)
 
-# Running on background
-while True:
-    files = os.listdir("./") # scan all files inside cwd
-    for fl in files:
-        if os.path.isfile(fl): # isfile only
-            ext = (fl.split(".")[-1]).lower() # split and convert extension to lowercase
-            if ext in vids:
-                shutil.move(fl, dl+folders[0]) # start moving 
-                print(fl,"has been moved to",dl+folders[0])
-            elif ext in imgs:
-                shutil.move(fl, dl+folders[1])
-                print(fl,"has been moved to",dl+folders[1])
-            elif ext in apks:
-                shutil.move(fl, dl+folders[2])
-                print(fl,"has been moved to",dl+folders[2])
-            elif ext in docx:
-                shutil.move(fl, dl+folders[3])
-                print(fl,"has been moved to",dl+folders[3])
-            elif ext in icons:
-                shutil.move(fl, dl+folders[4])
-                print(fl,"has been moved to",dl+folders[4])
-            elif ext in comps:
-                shutil.move(fl, dl+folders[5])
-                print(fl,"has been moved to",dl+folders[5])
-            else:
-                shutil.move(fl, dl+folders[6])
-                print(fl,"has been moved to",dl+folders[6])
-
+files = os.listdir("./") # scan all files inside cwd
+for fl in files:
+    if os.path.isfile(fl): # isfile only
+        ext = (fl.split(".")[-1]).lower() # split and convert extension to lowercase
+        if ext in vids:
+           shutil.move(fl, dl+folders[0]) # start moving 
+           print(fl,"has been moved to",dl+folders[0])
+        elif ext in imgs:
+            shutil.move(fl, dl+folders[1])
+            print(fl,"has been moved to",dl+folders[1])
+        elif ext in apks:
+            shutil.move(fl, dl+folders[2])
+            print(fl,"has been moved to",dl+folders[2])
+        elif ext in docx:
+            shutil.move(fl, dl+folders[3])
+            print(fl,"has been moved to",dl+folders[3])
+        elif ext in icons:
+            shutil.move(fl, dl+folders[4])
+            print(fl,"has been moved to",dl+folders[4])
+       elif ext in comps:
+            shutil.move(fl, dl+folders[5])
+            print(fl,"has been moved to",dl+folders[5])
+       else:
+            shutil.move(fl, dl+folders[6])
+            print(fl,"has been moved to",dl+folders[6])
 
 
 print("Save your time and you can chill!")
