928b410f12878935149d81e254cfbeff8afc9478
Ann <75046143+annazc-ann@users.noreply.github.com>
2020-12-17 14:27:22 +0700

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -1,20 +1,13 @@
 # import the required modules
 import os, shutil
+from styl import * # styling terminal output
 
 # Download path
 dl = "/sdcard/Download/"
 folders = ["Video","Images","Music","Apps","Documents","Icons","Compressed","Others"]
-
-# create destination folder 
-for i in range(len(folders)):
-   def mkdirs():
-      os.makedirs(dl+folders[i])
-   if os.path.exists(dl+folders[i]) == False:
-      mkdirs()
-      os.system("echo '\n\e[92mSuccessfully created folder:'")
-      print(folders[i]) 
-
-# Declaring files' formats with arrays
+   
+# declaring file formats with arrays
+# you can add your desired file format
 vids = ["mp4","mkv","avi","mov"]
 imgs = ["jpg","jpeg","img"]
 icons = ["png","ico"]
@@ -23,40 +16,46 @@ docx = ["docx","doc","pdf","epub","bok","pptx","xlsx"]
 apks = ["apk","xapk","exe"]
 comps = ["zip","rar","tar","gz","iso","7z","bz2","jar","lzma"]
 
-# Change the current working directory(cwd) to the Download path
+# create destination folder 
+for i in range(len(folders)):
+   if os.path.exists(dl+folders[i]) == False:
+      os.makedirs(dl+folders[i])
+      print(style.OKGREEN+"Successfully created folder:",style.OKBLUE+folders[i])
+      
+# change the current working directory (cwd) to dl path
+os.chdir(dl)
 
-files = os.listdir(dl) # scan all files inside cwd
+files = os.listdir("./") # scan all files inside cwd
 for fl in files:
-   for i in range(len(folders)):
-      if os.path.isfile(fl): # isfile only
-         ext = (fl.split(".")[-1]).lower() # split and convert extension to lowercase
-         try:
-            if ext in vids:
-               shutil.move(fl, dl+folders[i]) # start moving 
-               print(fl,"has been moved to",dl+folders[0])
-            elif ext in imgs:
-               shutil.move(fl, dl+folders[i])
-               print(fl,"has been moved to",dl+folders[1])
-            elif ext in auds:
-               shutil.move(fl, dl+folders[i])
-               print(fl,"has been moved to",dl+folders[2])
-            elif ext in apks:
-               shutil.move(fl, dl+folders[i])
-               print(fl,"has been moved to",dl+folders[3])
-            elif ext in docx:
-               shutil.move(fl, dl+folders[i])
-               print(fl,"has been moved to",dl+folders[4])
-            elif ext in icons:
-               shutil.move(fl, dl+folders[i])
-               print(fl,"has been moved to",dl+folders[5])
-            elif ext in comps:
-               shutil.move(fl, dl+folders[i])
-               print(fl,"has been moved to",dl+folders[6])
-            else:
-               shutil.move(fl, dl+folders[i]) 
-               print(fl,"has been moved to",dl+folders[7])
-         except:
-            print("Can't move file, file already exist") 
+   if os.path.isfile(fl): # isfile only
+      ext = (fl.split(".")[-1]).lower() # split and convert extension to lowercase
+      try:
+         if ext in vids:
+            shutil.move(fl, folders[0]) # start moving 
+            print(fl,"has been moved to",dl+folders[0])
+         elif ext in imgs:
+            shutil.move(fl, folders[1])
+            print(fl,"has been moved to",dl+folders[1])
+         elif ext in auds:
+            shutil.move(fl, folders[2])
+            print(fl,"has been moved to",dl+folders[2])
+         elif ext in apks:
+            shutil.move(fl, folders[3])
+            print(fl,"has been moved to",dl+folders[3])
+         elif ext in docx:
+            shutil.move(fl, folders[4])
+            print(fl,"has been moved to",dl+folders[4])
+         elif ext in icons:
+            shutil.move(fl, folders[5])
+            print(fl,"has been moved to",dl+folders[5])
+         elif ext in comps:
+            shutil.move(fl, folders[6])
+            print(fl,"has been moved to",dl+folders[6])
+         else:
+            shutil.move(fl, folders[7]) 
+            print(fl,"has been moved to",dl+folders[7])
+      except:
+         print(f"\n{style.FAIL}Can't move {fl}, already exist") 
 
 os.system('echo "\n\e[92mNow you can chill!\e[0m\n"')
 
