672f984f456feba9ce2dc9410ae96fa141a80203
Ann <75046143+annazc-ann@users.noreply.github.com>
2020-12-19 16:44:19 +0700

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -10,11 +10,14 @@ folders = ["Video","Images","Music","Apps","Documents","Icons","Compressed","Oth
 # you can add your desired file format
 vids = ["mp4","mkv","avi","mov"]
 imgs = ["jpg","jpeg","img"]
-icons = ["png","ico"]
 auds = ["mp3","aac","m4a"]
-docx = ["docx","doc","pdf","epub","bok","pptx","xlsx"]
 apks = ["apk","xapk","exe"]
-comps = ["zip","rar","tar","gz","iso","7z","bz2","jar","lzma"]
+docx = ["docx","doc","pdf","epub","bok","pptx","xlsx"]
+icons = ["png","ico"]
+comps = ["zip","rar","tar","gz","xz","iso","7z","bz2","jar","lzma"]
+
+formats = [vids,imgs,auds,apks,docx,icons,comps]
+
 exception = 'crdownload'
 
 # create destination folder 
@@ -33,39 +36,22 @@ def loop():
          hidden = fl.startswith('.')
          ext = (fl.split(".")[-1]).lower() # split and convert extension to lowercase
          try:
-            if hidden:
-               pass
-            elif ext in exception:
-               pass
-            elif ext in vids:
-               shutil.move(fl, folders[0]) # start moving 
-               print(fl,"has been moved to",dl+folders[0])
-            elif ext in imgs:
-               shutil.move(fl, folders[1])
-               print(fl,"has been moved to",dl+folders[1])
-            elif ext in auds:
-               shutil.move(fl, folders[2])
-               print(fl,"has been moved to",dl+folders[2])
-            elif ext in apks:
-               shutil.move(fl, folders[3])
-               print(fl,"has been moved to",dl+folders[3])
-            elif ext in docx:
-               shutil.move(fl, folders[4])
-               print(fl,"has been moved to",dl+folders[4])
-            elif ext in icons:
-               shutil.move(fl, folders[5])
-               print(fl,"has been moved to",dl+folders[5])
-            elif ext in comps:
-               shutil.move(fl, folders[6])
-               print(fl,"has been moved to",dl+folders[6])
-            else:
-               shutil.move(fl, folders[7]) 
-               print(fl,"has been moved to",dl+folders[7])
+            for i in range(len(folders)):
+                if hidden:
+                    pass
+                elif ext in exception:
+                    pass
+                elif ext in formats[i]:
+                    shutil.move(fl, folders[i]) # start moving 
+                    print(fl,"has been moved to",dl+folders[i])
+                else:
+                    shutil.move(fl,folders[-1])
+                    print(fl,"has been moved to",dl+folders[-1])
          except:
             for x in range(len(folders)):
-               exists = os.path.isfile(folders[x])
-               if exists:
-                  print(f"\n{style.FAIL}Can't move {fl}, already exist") 
+               exists = os.listdir(folders[x])
+               if fl in exists:
+                  print(f"\n{style.FAIL}Can't move {fl}, already exist"+style.ENDC) 
                   pass
 
 while True:
