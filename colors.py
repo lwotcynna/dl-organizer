1202283c15a8f1ff43e75020e415f9ebe5da78aa
Ann <75046143+annzc@users.noreply.github.com>
2021-01-13 18:03:04 +0700

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,9 @@
+class colorz:
+    HEAD = '\033[95m'
+    BLUE = '\033[94m'
+    OK = '\033[92m'
+    WARN = '\033[93m'
+    FAIL = '\033[91m'
+    O = '\033[0m'
+    BOLD = '\033[1m'
+    UL = '\033[4m'
